376755495286d745c4beefe661fc483d0582884a
LinhLe2307 <lemylinh23071997@gmail.com>
2022-03-10 20:08:23 +0200

    Add types and search for each character

--- main.js
+++ main.js
@@ -74,3 +74,49 @@ const getToTop = () => {
 fetchData();
 searchName.addEventListener("change", searchPokemon);
 getToTopBtn.addEventListener("click", getToTop);
+
+
+// class fetchWrapper {
+//   constructor(baseURL) {
+//     this.baseURL = baseURL;
+//   }
+//   get(endpoint) {
+//     return fetch(this.baseURL + endpoint).then((request) => request.json());
+//   }
+// }
+
+// function fetchData() {
+//   const pokemon = new fetchWrapper("https://pokeapi.co/api/v2/pokemon");
+//   const container = document.querySelector(".container");
+//   pokemon.get("?offset=0&limit=50").then((data) => {
+//     data.results.map((item) => {
+//       const cardTitle = document.createElement("h2");
+//       const cardName = document.createElement("div");
+//       const card = document.createElement("div");
+
+//       cardTitle.appendChild(document.createTextNode(item.name));
+//       cardName.appendChild(cardTitle);
+
+//       cardTitle.classList.add("card-title");
+//       cardName.classList.add("card-name");
+//       card.classList.add("card");
+
+//       fetchImage(item, card);
+//       card.appendChild(cardName);
+//       container.appendChild(card);
+//     });
+//     console.log(data);
+//   });
+// }
+
+// function fetchImage(item, card) {
+//   const img = document.createElement("img");
+//   new fetchWrapper(item.url)
+//     .get("")
+//     .then((data) =>
+//       img.setAttribute("src", data.sprites.other.dream_world.front_default)
+//     );
+//   card.appendChild(img);
+// }
+
+// fetchData();
